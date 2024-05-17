f80e893e250f61f094a32a50d1c040f347e207b8
Adam Duke <adam@dukemotorsports.net>
2024-05-16 19:59:01 -0700

    Fix minimalistic check

--- src/Scripts/Game/RespawnSystem/SCR_SpawnRequestComponent.c
+++ src/Scripts/Game/RespawnSystem/SCR_SpawnRequestComponent.c
@@ -5,7 +5,7 @@ modded class SCR_SpawnRequestComponent
 	{
 		// Mute warnings on current minimalistic EPF respawn system "hotfix"
 		SCR_RespawnSystemComponent respawnSystem = SCR_RespawnSystemComponent.GetInstance();
-		if (respawnSystem && respawnSystem.IsInherited(EPF_BaseRespawnSystemComponent))
+		if (respawnSystem && respawnSystem.GetSpawnLogic() && respawnSystem.GetSpawnLogic().IsInherited(EPF_BaseSpawnLogic))
 			return;
 
 		super.OnPostInit(owner);
